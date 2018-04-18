6aa700e8e4bc62142c6734b6a978243ab5bd935a
Christopher Wrigley <1005943@student.sae.edu.au>
2018-04-18 23:32:47 +1000

    syncing

---
+++ Assets/Enemy_AI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_AI : MonoBehaviour
+{
+    public GameObject player;
+
+    public float speed;
+
+    public float detection_distance;
+
+    public int health = 100;
+
+    private float distance;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distance < detection_distance)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+        }
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    public void Hit (int damage)
+    {
+        Debug.Log("ouch");
+        health -= damage;
+    }
+}
---
+++ Assets/Gun.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gun : MonoBehaviour
+{
+    public bool isactive;
+
+    public GameObject gun;
+
+    public GameObject pickup_text;
+
+    public GameObject reload_text;
+    public Text ammunition_text;
+    public int ammunition;
+    private bool reloading;
+
+    private Ray ray;
+
+    private RaycastHit hit;
+
+    // Use this for initialization
+    void Start ()
+    {
+        if(isactive)
+        {
+            Pickup();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!isactive)
+        {
+            ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            if (Physics.Raycast(ray, out hit, 10))
+            {
+                if (hit.transform.gameObject.tag == "gun")
+                {
+                    pickup_text.SetActive(true);
+                    if(Input.GetKeyDown(KeyCode.E))
+                    {
+                        Pickup();
+                        Destroy(hit.transform.gameObject);
+                    }
+                }
+                else
+                {
+                    pickup_text.SetActive(false);
+                }
+
+            }
+        }
+
+		if (Input.GetButtonDown("Fire1") && isactive && !reloading && ammunition > 0)
+        {
+            Debug.Log("Bang");
+            Fire();
+        }
+
+        if(Input.GetKeyDown(KeyCode.R) && isactive && !reloading)
+        {
+            reloading = true;
+            StartCoroutine("Reload");
+        }
+
+        ammunition_text.text = ammunition.ToString();
+
+        if(ammunition < 3)
+        {
+            reload_text.SetActive(true);
+        }
+        else
+        {
+            reload_text.SetActive(false);
+        }
+	}
+
+    IEnumerator Reload ()
+    {
+        yield return new WaitForSeconds(1.0f);
+        reloading = false;
+        ammunition = 10;
+    }
+
+    void Fire ()
+    {
+        ammunition--;
+        ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            if(hit.transform.gameObject.tag == "enemy")
+            {
+                Debug.Log("Hit");
+                Enemy_AI enemey = hit.transform.gameObject.GetComponent<Enemy_AI>();
+                enemey.Hit(25);
+            }
+            else
+            {
+                Debug.Log("Miss");
+            }
+        }
+    }
+
+    void Pickup ()
+    {
+        isactive = true;
+        ammunition_text.enabled = true;
+        gun.SetActive(true);
+        pickup_text.SetActive(false);
+    }
+}
