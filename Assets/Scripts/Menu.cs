e4a60c6639347fd7c953ff774f60f6184f900ce8
Christopher Wrigley <1005943@student.sae.edu.au>
2018-04-27 18:37:33 +1000

    just some fixes

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,6 +7,12 @@ public class Menu : MonoBehaviour
 {
     public string levelname;
 
+    void Update ()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void Play ()
     {
         SceneManager.LoadScene(levelname);
